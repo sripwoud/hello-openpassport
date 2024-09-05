243dd6821517b82789b7226cfb328c91eb5987ed
sripwoud <me@sripwoud.xyz>
2024-09-05 10:43:11 +0200

    failed verification

--- src/app/api/verify/route.ts
+++ src/app/api/verify/route.ts
@@ -1,9 +1,8 @@
+import { verifier } from 'l/verifier'
 import { type NextRequest, NextResponse } from 'next/server'
-// import { verifier } from 'l/verifier'
 
 export async function POST(req: NextRequest) {
   const proof = await req.json()
-  // const { valid }  = await verifier.verify(proof)
-  // return NextResponse.json(valid)
-  return NextResponse.json(true)
+  const { valid } = await verifier.verify(proof)
+  return NextResponse.json(valid)
 }
